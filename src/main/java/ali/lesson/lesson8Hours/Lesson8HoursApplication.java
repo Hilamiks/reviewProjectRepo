9a26ca5b0cf69b3476d8738aec3af99444d887e0
Mike Sokolov <miha13.sokolov@gmail.com>
2024-01-10 00:51:38 +0500

    initial commit

---
+++ src/main/java/ali/lesson/lesson8Hours/Lesson8HoursApplication.java
@@ -0,0 +1,13 @@
+package ali.lesson.lesson8Hours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson8HoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson8HoursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ali/lesson/lesson8Hours/Lesson8HoursApplicationTests.java
@@ -0,0 +1,13 @@
+package ali.lesson.lesson8Hours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson8HoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
