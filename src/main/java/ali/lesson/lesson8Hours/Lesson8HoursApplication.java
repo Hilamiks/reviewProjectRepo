86e31df043ee2adbe9cec9edfac6fae76cfdf74a
Mike Sokolov <miha13.sokolov@gmail.com>
2024-01-10 01:11:54 +0500

    using beans

--- src/main/java/ali/lesson/lesson8Hours/Lesson8HoursApplication.java
+++ src/main/java/ali/lesson/lesson8Hours/Lesson8HoursApplication.java
@@ -2,12 +2,27 @@ package ali.lesson.lesson8Hours;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Lesson8HoursApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lesson8HoursApplication.class, args);
+		var ctx = SpringApplication.run(Lesson8HoursApplication.class, args);
+
+//		OLD WAY
+//		MyClass myClass = new MyClass();
+//		System.out.println(myClass.sayHello());
+//
+		//BEAN WAY
+		MyClass myClass = ctx.getBean(MyClass.class);
+		System.out.println(myClass.sayHello());
+
+	}
+
+	//@Bean
+	public MyClass myClass() {
+		return new MyClass();
 	}
 
 }
---
+++ src/main/java/ali/lesson/lesson8Hours/MyClass.java
@@ -0,0 +1,12 @@
+package ali.lesson.lesson8Hours;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyClass {
+
+	public String sayHello() {
+		return "Hello from myClass!";
+	}
+
+}
