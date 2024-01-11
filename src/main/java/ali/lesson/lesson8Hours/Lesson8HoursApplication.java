a138667d7372678396df363bd3a9aba05440ee35
Mike Sokolov <miha13.sokolov@gmail.com>
2024-01-11 22:00:50 +0500

    starting working on API

--- src/main/java/ali/lesson/lesson8Hours/Lesson8HoursApplication.java
+++ src/main/java/ali/lesson/lesson8Hours/Lesson8HoursApplication.java
@@ -10,16 +10,6 @@ import java.util.Collections;
 public class Lesson8HoursApplication {
 
 	public static void main(String[] args) {
-		var app = new SpringApplication(Lesson8HoursApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active","custom"));
-		var ctx = app.run(args);
-
-//		OLD WAY
-//		MyClass myClass = new MyClass();
-//		System.out.println(myClass.sayHello());
-//
-		//BEAN WAY
-		MyService myService = ctx.getBean(MyService.class);
-		System.out.println(myService.tellStory());
+		SpringApplication.run(Lesson8HoursApplication.class);
 	}
 }
